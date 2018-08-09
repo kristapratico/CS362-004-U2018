c0c28e00341211503dd8cb9c9efdc6df9e20b597
kpratico <praticok@oregonstate.edu>
2018-08-08 22:19:49 -0700

    added authority partition code

--- projects/praticok/FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ projects/praticok/FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -1,5 +1,4 @@
 
-
 import junit.framework.TestCase;
 
 //You can use this as a skeleton for your 3 different test approach
@@ -10,9 +9,9 @@ import junit.framework.TestCase;
 
 
 
+
 public class UrlValidatorTest extends TestCase {
-
-
+	
    public UrlValidatorTest(String testName) {
       super(testName);
    }
@@ -21,34 +20,57 @@ public class UrlValidatorTest extends TestCase {
    
    public void testManualTest()
    {
-//You can use this function to implement your manual testing	   
-	   UrlValidator url = new UrlValidator(UrlValidator.ALLOW_ALL_SCHEMES);
+   
+	   UrlValidator url = new UrlValidator();
 	   
-	   System.out.println(url.isValid("http://www.neopets.com"));
+	   assertTrue(url.isValid("http://www.neopets.com"));
+	   System.out.println(url.isValid("http://"));
    }
    
    
    public void testScheme()
    {
 	   String url = "www.neopets.com";
-	   UrlValidator testUrl = new UrlValidator();
+	   UrlValidator testUrl = new UrlValidator(UrlValidator.ALLOW_ALL_SCHEMES);
 	   
 	   String[] validScheme = {"http://", "ftp://", "h3t://", "https://", "ftp://", ""};
        String[] invalidScheme = {"3ht://", "http:/", "http:", "http/", "://"};
        System.out.println("Partition Testing: Scheme");
-       System.out.println("Valid Schemes");
+       System.out.println("\n" + "Valid Schemes");
        for (int x = 0; x < validScheme.length; x++) {
     	   System.out.println(validScheme[x] + url + ": " + testUrl.isValid(validScheme[x] + url));
+    	   //assertEquals(true, testUrl.isValid(validScheme[x] + url));
+
        }
-       System.out.println("Invalid Schemes");
+       
+       System.out.println("\n" + "Invalid Schemes");
        for (int x = 0; x < invalidScheme.length; x++) {
     	   System.out.println(invalidScheme[x] + url + ": " + testUrl.isValid(invalidScheme[x] + url));
+    	   //assertEquals(true, testUrl.isValid(invalidScheme[x] + url));
        }
        
    }
    
-   public void testYourSecondPartition(){
-		 //You can use this function to implement your Second Partition testing	   
+   public void testAuthority()
+   {
+	   String url = "http://";
+	   UrlValidator testUrl = new UrlValidator(UrlValidator.ALLOW_ALL_SCHEMES);
+	   
+	   String[] validAuth = {"www.google.com", "www.neopets.com", "0.0.0.0", "256.255.255.256", "www.amazon.com", "go.com"};
+       String[] invalidAuth = {"zyx", ".1.2.3", "go.b", "yyy.", ""};
+       System.out.println("\n" + "Partition Testing: Authority");
+       System.out.println("\n" + "Valid Authoritys");
+       for (int x = 0; x < validAuth.length; x++) {
+    	   System.out.println(url + validAuth[x] + ": " + testUrl.isValid(url + validAuth[x]));
+    	   //assertEquals(true, testUrl.isValid(validAuth[x] + url));
+
+       }
+       
+       System.out.println("\n" + "Invalid Authoritys");
+       for (int x = 0; x < invalidAuth.length; x++) {
+    	   System.out.println(url + invalidAuth[x] + ": " + testUrl.isValid(url + invalidAuth[x]));
+    	   //assertEquals(true, testUrl.isValid(invalidAuth[x] + url));
+       }
 
    }
    //You need to create more test cases for your Partitions if you need to 
